8fac64c1185996cbb3527a0d0c00043c0e0e2fb0
Derek Daniels <dbdaniel@mtu.edu>
2016-02-14 00:56:04 -0500

    Creates the login screen and starts it first

--- app/src/main/java/com/example/derek/drunkdial/Login.java
+++ app/src/main/java/com/example/derek/drunkdial/Login.java
@@ -19,18 +19,12 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
+        setContentView(R.layout.activity_login);
     }
 
-    public void login(View v) {
+    public void login(View view) {
 
-        String name = ((EditText)findViewById(R.id.editTextName)).getText().toString();
-        String username = ((EditText)findViewById(R.id.editTextUsername)).getText().toString();
-        String pass = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
-        String phone = ((EditText)findViewById(R.id.editTextPhonenumber)).getText().toString();
-
-        System.out.println(DrunkDatabase.testAddUser(name, username, pass, phone));
-        System.out.println(DrunkDatabase.testAddUser(name, username, pass, phone));
+        System.out.println("Clicked the button");
 
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
